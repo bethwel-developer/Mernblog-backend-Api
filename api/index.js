f4f4ef55b5cd994a23d42316f4e7315d92b8f595
bethwel-developer <bethwelnjenga237@gmail.com>
2023-06-08 15:18:11 -0700

    initial commit

---
+++ api/index.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
+const register = require ("./routes/auth")
+const login = require ("./routes/auth")
+
+const categoryRoute = require("./routes/categories");
+const cors = require ("cors");
+const multer = require("multer");
+const path = require("path");
+const port = 5000
+
+app.use(express.json())
+app.use(cors())
+require("dotenv").config();
+app.use("/images", express.static(path.join(__dirname, "/images")));
+
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+
+mongoose.connect(process.env.MONGO_URI,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+  }).then(() => {
+    console.log(`connected to mongodb`)
+  }).catch((err) => {
+    console.log(err.message)
+  });
+  
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File has been uploaded");
+});
+
+app.use("/api/auth", register);
+app.use("/api/auth", login);
+
+
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+
+app.use("/api/categories", categoryRoute);
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
---
+++ api/routes/auth.js
@@ -0,0 +1,47 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+
+//REGISTER
+router.post("/register", async (req, res) => {
+  try {
+    
+    const { username, email, password } = req.body;
+    const usernameCheck = await User.findOne({ username });
+    if (usernameCheck)
+      return res.json({ msg: "Username already used", status: false });
+    const emailCheck = await User.findOne({ email });
+    if (emailCheck)
+      return res.json({ msg: "Email already used", status: false });
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({
+      email,
+      username,
+      password: hashedPassword,
+    });
+    return res.json({ status: true, user });
+  } catch (error) {
+    res.status(500).json(err); 
+  }
+})
+
+//login
+
+router.post("/login",  async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    if (!user)
+      return res.json({ msg: "Incorrect Username or Password", status: false });
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid)
+      return res.json({ msg: "Incorrect Username or Password", status: false });
+
+      return res.json({ status: true, user });
+  } catch (error) {
+    res.status(500).json(err); 
+  }
+})
+
+
+module.exports = router;
