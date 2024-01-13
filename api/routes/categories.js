9eb5d09df1835846e8c602112e760dc82d8927af
Bethwel Njenga <bethwelnjenga237@gmail.com>
2024-01-13 19:07:54 +0300

    Update categories.js

--- api/routes/categories.js
+++ api/routes/categories.js
@@ -1,7 +1,7 @@
  const router = require("express").Router();
  const Category = require("../models/Category");
 
-router.post("/", async (req, res) => {
+ router.post("/", async (req, res) => {
   const newCat = new Category(req.body);
   try {
     const savedCat = await newCat.save();
